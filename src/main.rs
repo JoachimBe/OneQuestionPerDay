dedba52186ad3253d018513cc3025b5e322e97bd
unknown <j.bertrand0032@gmail.com>
2026-04-08 00:12:02 +0200

    add result to ensure user entry

--- src/main.rs
+++ src/main.rs
@@ -1,33 +1,68 @@
 use std::io;
 
 fn main() {
-    println!("enter RGBA value, separate with a comma: ");
-
-    //get user entry
+    println!("entrer une valeur RGBA séparer par une virgule: ");
     let mut user_entry: String= String::new();
     io::stdin().read_line(&mut user_entry).expect("Failed to readline");
+   // println!("{}", user_entry);
+   let mut users_rgbacolor: Vec<f32> = Vec::new();
+   for color in user_entry.split(","){
+       users_rgbacolor.push(color.trim().parse().expect("falied to parse as f32"))
+    }
 
-    //extract value from user entry and store it in a vector
-   let mut users_rgba_color: Vec<f32> = Vec::new();
-     for color in user_entry.split(","){
-        users_rgba_color.push(color.trim().parse().expect("falied to parse as f32"))
-     }
+    let user_color =  RGBA::new( 
+        users_rgbacolor[0],
+        users_rgbacolor[1],
+        users_rgbacolor[2],
+        users_rgbacolor[3]);
 
-    //setting background value to 0, used in calculation 
     let background_red:f32= 0.0;
     let background_green:f32= 0.0;
     let background_blue:f32= 0.0;
     let background_colors =  vec![background_red, background_green, background_blue];
 
-     blend_color(users_rgba_color, background_colors)
+    
+
+    //print!("\x1b[38;2;{user_rgbacolor[0]};{user_rgbacolor[1]};{char_blue}m ■\x1b[0m");
+     blend_color(user_color.expect(" failed to send RGBA to function").get_color(), background_colors)
+}
+//(1-a) × Cf + a × C
+fn blend_color(cf: Vec<f32>, c: Vec<f32>){
+    let a = cf[3];
+    let r = ((1.0 -a) * cf[0] + a *  c[0]) as u8;
+    let g = ((1.0 -a) * cf[1] + a *  c[1]) as u8;
+    let b = ((1.0 -a) * cf[2] + a *  c[2]) as u8;
+    print!("\x1b[38;2;{r};{g};{b}m ■\x1b[0m");
+    //return vec![r,g,b]
 }
 
-fn blend_color(front_color: Vec<f32>, background_color: Vec<f32>){
-    let a = front_color[3];
 
-    //Math for color balancing with alpha
-    let r = ((1.0 -a) * front_color[0] + a *  background_color[0]) as u8;
-    let g = ((1.0 -a) * front_color[1] + a *  background_color[1]) as u8;
-    let b = ((1.0 -a) * front_color[2] + a *  background_color[2]) as u8;
-    print!("\x1b[38;2;{r};{g};{b}m ■\x1b[0m");
-} 
+pub struct RGBA{
+    red: f32,
+    green: f32,
+    blue: f32,
+    alpha: f32,
+}
+
+impl RGBA{
+    pub fn new(red: f32, green: f32, blue: f32, alpha: f32) ->Result<RGBA, &'static str>{
+        let colors_vec = vec![red,green,blue];
+        for color in colors_vec {
+            if color < 0.0 || color > 255.0{
+                return Err("One or many value of the RGB are incorect, please specify a value between 0 and 255")
+            }else if alpha < 0.0 || alpha > 1.0{
+                return Err(" Alpha value should be between 0.0 and 1.0")
+            }
+        }
+        Ok(RGBA{
+            red,
+            green,
+            blue,
+            alpha,
+        })
+        
+    }
+    pub fn get_color(&self)-> Vec<f32>{
+        vec![self.red, self.green, self.blue, self.alpha]
+    }
+}
