4751ea06944f5412cc3117c851631d386f569f79
unknown <j.bertrand0032@gmail.com>
2026-04-07 23:34:01 +0200

    set led color with rgba

--- src/main.rs
+++ src/main.rs
@@ -1,29 +1,33 @@
 use std::io;
 
 fn main() {
-    println!("entrer une valeur RGBA séparer par une virgule: ");
+    println!("enter RGBA value, separate with a comma: ");
+
+    //get user entry
     let mut user_entry: String= String::new();
     io::stdin().read_line(&mut user_entry).expect("Failed to readline");
-   // println!("{}", user_entry);
-   let mut users_rgbacolor: Vec<f32> = Vec::new();
+
+    //extract value from user entry and store it in a vector
+   let mut users_rgba_color: Vec<f32> = Vec::new();
      for color in user_entry.split(","){
-        users_rgbacolor.push(color.trim().parse().expect("falied to parse as f32"))
+        users_rgba_color.push(color.trim().parse().expect("falied to parse as f32"))
      }
 
+    //setting background value to 0, used in calculation 
     let background_red:f32= 0.0;
     let background_green:f32= 0.0;
     let background_blue:f32= 0.0;
     let background_colors =  vec![background_red, background_green, background_blue];
 
-    //print!("\x1b[38;2;{user_rgbacolor[0]};{user_rgbacolor[1]};{char_blue}m ■\x1b[0m");
-     calcul_melange_couleur(users_rgbacolor, background_colors)
+     blend_color(users_rgba_color, background_colors)
 }
-//(1-a) × Cf + a × C
-fn calcul_melange_couleur(cf: Vec<f32>, c: Vec<f32>){
-    let a = cf[3];
-    let r = ((1.0 -a) * cf[0] + a *  c[0]) as u8;
-    let g = ((1.0 -a) * cf[1] + a *  c[1]) as u8;
-    let b = ((1.0 -a) * cf[2] + a *  c[2]) as u8;
-    print!("\x1b[38;2;{r};{g};{b}m ■");
-    //return vec![r,g,b]
-}
+
+fn blend_color(front_color: Vec<f32>, background_color: Vec<f32>){
+    let a = front_color[3];
+
+    //Math for color balancing with alpha
+    let r = ((1.0 -a) * front_color[0] + a *  background_color[0]) as u8;
+    let g = ((1.0 -a) * front_color[1] + a *  background_color[1]) as u8;
+    let b = ((1.0 -a) * front_color[2] + a *  background_color[2]) as u8;
+    print!("\x1b[38;2;{r};{g};{b}m ■\x1b[0m");
+} 
