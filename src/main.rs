eb4212c4a4296855bb935d548f9613b4b6f9a526
unknown <j.bertrand0032@gmail.com>
2026-04-13 13:09:43 +0200

    first function created

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,17 @@
+use std::{thread, time};
+use rand::RngExt;
+
 fn main() {
-    println!("Hello, world!");
+    random_number_generator_thread()
+}
+
+fn random_number_generator_thread(){
+    let time_in_millis = time::Duration::from_millis(1000); 
+    loop{
+        thread::spawn(move || {
+            let mut random_number = rand::rng();
+            let random_number: u8 =  random_number.random_range(0..255);
+            thread::sleep(time_in_millis);
+        });
+    };
 }
