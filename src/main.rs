4a6c9dd3bec27ec107e35742bb3a953f6a6e9507
unknown <j.bertrand0032@gmail.com>
2026-04-15 19:14:09 +0200

    before trying async

--- src/main.rs
+++ src/main.rs
@@ -1,30 +1,35 @@
 use std::time::Duration;
 use std::{thread, time};
-use std::sync::mpsc;
+use std::io::{Write, stdout};
+use std::io;
+use std::sync::{mpsc, Arc, Mutex};
 
-use crossterm::event::{self, Event, KeyCode, KeyEventKind};
+use std::future::Future;
+
+use crossterm::event::{self, Event, KeyCode, KeyEventKind, poll};
 //use raw_input::{Core,Event,Key, Listen};
 use rand::RngExt;
 
-fn main()  {
+fn main() -> io::Result<()> {
     
     let time_in_millis = time::Duration::from_millis(50); 
     let mut vec_of_bytes: Vec<u8> = vec![];
     //let new_event = Event{KeyDown{key: }};
-    let mut is_pressed= false;
+    //let mut arc_is_pressed= Arc::new(Mutex::new(false));
+    let mut is_pressed = false;
 
     while is_pressed!=true{
         
         vec_of_bytes.push(generate_random_number_in_thread(time_in_millis));
-        
-        thread::sleep(time::Duration::from_millis(500));
-        is_pressed = listen_to_key_stroke_cross(vec_of_bytes.clone());
+        println!("test {vec_of_bytes:?}");
+        //thread::sleep(time::Duration::from_millis(500));
+        is_pressed = listen_to_key_stroke_cross();
        
        //is_pressed = listen_to_key_stroke(vec_of_bytes.clone());
        
        
     };
-
+    Ok(())
 }
 
 fn generate_random_number_in_thread(time_in_millis: time::Duration) -> u8{
@@ -44,21 +49,19 @@ fn generate_random_number_in_thread(time_in_millis: time::Duration) -> u8{
         received
 }
 
-fn listen_to_key_stroke_cross(mut vec_of_bytes: Vec<u8>) -> bool{
-        let (tx, rx) = mpsc::channel::<bool>();
-        let new_vec = vec_of_bytes.clone();
-        thread::spawn( move || {
+ fn listen_to_key_stroke_cross() -> bool {
+         thread::spawn( move || {
             
-            thread::sleep(time::Duration::from_millis(500));
-            if let Event::Key(key_event) = event::read().expect("failed to read event"){
-                match key_event{
-                    key_event_kind =>{tx.send(true).expect("send keydown failed") ;println!("key{key_event_kind:?} vec{:?}", new_vec.clone());},
+           if let  Event::Key(key_event) = event::read().expect("failed to read event"){
+                match key_event {
+                    key_event_kind =>{
+                        if key_event_kind.is_release()==true{}else{println!("key {key_event_kind:?}");}},
                 }
-            }
-        });
-        let received = rx.recv().expect(" receiver keydown failed");
-        received
-
+            } 
+        }); 
+        false
+        //poll(Duration::from_secs(0))
+            
 }
 
 /*fn listen_to_key_stroke(mut vec_of_bytes: Vec<u8>) -> bool{
@@ -77,7 +80,6 @@ fn listen_to_key_stroke_cross(mut vec_of_bytes: Vec<u8>) -> bool{
         });
         
         let received:bool = rx.recv().expect("receiver failed");
-        //thread::sleep(time::Duration::from_millis(500));
         Listen::stop(); // Stop Listen
         Core::stop();
     received
