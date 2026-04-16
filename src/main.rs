1a6f6ce726795cafa8b2020d4413121a3eed46b1
unknown <j.bertrand0032@gmail.com>
2026-04-16 23:58:32 +0200

    first push

---
+++ src/main.rs
@@ -0,0 +1,79 @@
+//for UDP
+use std::net::UdpSocket;
+
+//for generating the byte array
+use std::time::Duration;
+use std::{thread, time};
+use std::io;
+use std::sync::{mpsc};
+
+use crossterm::event::{self, Event, KeyCode, KeyEventKind, poll};
+use rand::RngExt;
+
+fn main() {
+    loop{
+
+        
+        let fut_listen = listen_udp_port_3615();
+        fut_listen
+    }
+}
+
+ fn listen_udp_port_3615(){
+
+    
+        let socket = UdpSocket::bind("127.0.0.1:3615").expect("failed to create socket, couldn't bind to address");
+        let mut listen_buffer:[u8;200]= [0;200];
+
+        let (number_of_bytes, src_addr) = socket.recv_from(&mut listen_buffer).expect("couldn't write on the buffer");
+        
+        let filled_buffer =&mut listen_buffer[..number_of_bytes];
+        println!("{} {:?}", src_addr, filled_buffer);
+    
+}
+
+
+
+fn gen_array() -> io::Result<()> {
+    
+    let time_in_millis = time::Duration::from_millis(50); 
+    loop{
+        let mut vec_of_bytes: Vec<u8> = vec![];
+            println!("numbers are generated please press a key to show the range");
+
+        loop{
+            vec_of_bytes.push(generate_random_number_in_thread(time_in_millis));
+            
+            //event::poll permet d'attendre un evenement sans bloquer le thread 
+            if event::poll(Duration::from_millis(100)).unwrap() {
+                if let Event::Key(key_event_kind) = event::read().unwrap() {    // si l'event est lu correctement on effectue le block{ }
+                    if key_event_kind.is_release()==true{
+                        continue;
+                    }else{
+                        break;
+                    }                    
+                }
+            }
+        };
+        for byte in &vec_of_bytes{
+            print!("{byte} ");
+        }
+        println!(" {vec_of_bytes:?}");
+    }
+}
+
+
+fn generate_random_number_in_thread(time_in_millis: time::Duration) -> u8{
+    //variables qui permettent d'envoyer des données entre les threads
+    let (tx, rx) = mpsc::channel::<u8>();
+
+        thread::spawn(move || {
+            let mut random_number = rand::rng();
+            let random_number: u8 =  random_number.random_range(0..255);
+            tx.send(random_number).unwrap();
+        });
+        
+        thread::sleep(time_in_millis);
+        let received: u8 = rx.recv().unwrap();
+        received
+}
