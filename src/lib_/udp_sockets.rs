d13058dee3005a32852720e1bd9ce1cad799aa26
unknown <j.bertrand0032@gmail.com>
2026-04-23 01:56:58 +0200

    small update

--- src/lib_/udp_sockets.rs
+++ src/lib_/udp_sockets.rs
@@ -8,6 +8,11 @@ pub fn create_sender_udp_port(vec_of_bytes: Vec<u8>){
 pub fn create_listen_udp_port_3615()-> [u8;200]{
 
         let socket = UdpSocket::bind("127.0.0.1:3615").expect("failed to create socket, couldn't bind to address");
+        /*let mut size_buffer= [0u8;4];//tampon pour peeker la taille
+        socket.peek(&mut size_buffer).expect("peek size buffer failed");
+
+        let packet_len = u32::from_be_bytes(size_buffer) as usize;*/
+
         let mut listen_buffer:[u8;200]= [0;200]; //buffer of 200 bytes maximum
 
         //destructuring  response
--- src/main.rs
+++ src/main.rs
@@ -16,19 +16,29 @@ fn main() {
     });
 
     let mut received_byte = VecDeque::from(udp_sockets::create_listen_udp_port_3615());
-    while red.is_none() || green.is_none() ||  blue.is_none(){
-        if let Some(byte) = received_byte.pop_front(){
-            if red.is_none(){
-                red = Some(byte);
-            } else if green.is_none(){
-                green = Some(byte);
-            } else if blue.is_none(){
-                blue = Some(byte);
+    while !received_byte.is_empty(){
+        while red.is_none() || green.is_none() ||  blue.is_none(){
+            if let Some(byte) = received_byte.pop_front(){
+                if red.is_none(){
+                    red = Some(byte);
+                } else if green.is_none(){
+                    green = Some(byte);
+                } else if blue.is_none(){
+                    blue = Some(byte);
+                }
+            } else{
+                if red.is_none(){
+                    red = Some(0);
+                } else if green.is_none(){
+                    green = Some(0);
+                } else if blue.is_none(){
+                    blue = Some(0);
+                }
+                break;
             }
-        } else{
-            break;
         }
+        print!("\x1b[38;2;{};{};{}m■\x1b[0m", red.unwrap(), green.unwrap(), blue.unwrap());
+        (red,green,blue)  = (None,None,None)
     }
-    print!("\x1b[38;2;{};{};{}m■", red.unwrap(), green.unwrap(), blue.unwrap()); 
 
 }
